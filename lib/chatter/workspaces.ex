a9692000d8b48804d5649eefcc7c62a939c0492f
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-12 16:42:28 +0300

    tested workspaces context

--- lib/chatter/workspaces.ex
+++ lib/chatter/workspaces.ex
@@ -1,21 +1,47 @@
 defmodule Chatter.Workspaces do
+  alias Chatter.Repo
   alias Chatter.Models.Workspace
+  alias Chatter.Accounts.User
 
   def change_workspace(%Workspace{} = _workspace, _attrs \\ %{}) do
   end
 
-  def list_workspaces_for_user(_user_id) do
+  def list_workspaces_for_user(%User{} = user) do
+    user = Repo.preload(user, :workspaces)
+    user.workspaces
   end
 
-  def get_workspace(_id) do
+  def get_workspace(id), do: Repo.get!(Workspace, id)
+
+  def create_workspace(attrs \\ %{}) do
+    %Workspace{}
+    |> Workspace.changeset(attrs)
+    |> Repo.insert()
   end
 
-  def create_workspace(_attrs \\ %{}) do
+  def create_workspace_with_user(attrs \\ %{}, %User{} = user) do
+    {:ok, workspace} = create_workspace(attrs)
+
+    Repo.preload(workspace, :users)
+    |> Workspace.changeset_update_user(user, :admin)
+    |> Repo.update()
+
+    {:ok, workspace}
   end
 
-  def update_workspace(%Workspace{} = _workspace, _attrs) do
+  def add_user_in_workspace(%Workspace{} = workspace, %User{} = user, role) do
+    Repo.preload(workspace, :users)
+    |> Workspace.changeset_update_user(user, role)
+    |> Repo.update()
+
+    {:ok, workspace}
   end
 
-  def delete_wokspace(%Workspace{} = _workspace) do
+  def update_workspace(%Workspace{} = workspace, attrs) do
+    workspace
+    |> Workspace.changeset(attrs)
+    |> Repo.update()
   end
+
+  def delete_workspace(%Workspace{} = workspace), do: Repo.delete(workspace)
 end
--- test/chatter/workspaces_test.exs
+++ test/chatter/workspaces_test.exs
@@ -1,28 +1,138 @@
 defmodule Chatter.WorkspacesTest do
   use Chatter.DataCase
 
-  describe "list_workspaces_for_user/1" do
-    test "returns the workspaces for the logged in user" do
+  import Chatter.AccountsFixtures
+  import Chatter.WorkspacesFixtures
+
+  alias Chatter.Workspaces
+  alias Chatter.Models.Workspace
+
+  describe "create_workspace/1" do
+    test "creates a workspace and generates an icon" do
+      user = user_fixture()
+      name = "Workspace 1"
+
+      assert {:ok, %Workspace{} = workspace} =
+               Workspaces.create_workspace(%{
+                 name: name,
+                 creator_id: user.id
+               })
+
+      assert workspace.name == name
+      assert workspace.icon_path == "/images/workspace_icons/workspace_1_icon.jpg"
+      assert workspace.creator_id == user.id
+    end
+
+    test "refuses to create a workspace without creator_id param" do
+      assert {:error, %Ecto.Changeset{}} = Workspaces.create_workspace(%{name: "Workspace 1"})
+    end
+  end
+
+  describe "create_workspace_with_user/2" do
+    test "creates a workspace and sets the creator as admin" do
+      user = user_fixture()
+
+      assert {:ok, %Workspace{} = workspace} =
+               Workspaces.create_workspace_with_user(
+                 %{
+                   name: "Workspace 1",
+                   creator_id: user.id
+                 },
+                 user
+               )
+
+      assert hd(Repo.preload(workspace, [:users]).users) == user
+      assert hd(Repo.preload(workspace, [:users]).members).user_id == user.id
+    end
+  end
+
+  describe "add_user_in_workspace/2" do
+    test "creates a workspace with a user and adds another one" do
+      user = user_fixture()
+      user2 = user_fixture()
+
+      {:ok, %Workspace{} = workspace} =
+        Workspaces.create_workspace_with_user(
+          %{
+            name: "Workspace 1",
+            creator_id: user.id
+          },
+          user
+        )
+
+      assert {:ok, %Workspace{} = workspace} =
+               Workspaces.add_user_in_workspace(workspace, user2, :member)
+
+      assert length(Repo.preload(workspace, :users).users) == 2
     end
   end
 
   describe "get_workspace!/1" do
     test "returns the workspace with the given id" do
+      user = user_fixture()
+      workspace = workspace_fixture(%{creator_id: user.id})
+
+      assert Workspaces.get_workspace(workspace.id) == workspace
     end
   end
 
-  describe "create_workspace/1" do
-    test "creates a workspace" do
+  describe "list_workspaces_for_user/1" do
+    test "returns the workspaces for the logged in user" do
+      user = user_fixture()
+
+      {:ok, workspace1} =
+        Workspaces.create_workspace_with_user(
+          %{
+            name: "Workspace 1",
+            creator_id: user.id
+          },
+          user
+        )
+
+      {:ok, workspace2} =
+        Workspaces.create_workspace_with_user(
+          %{
+            name: "Workspace 2",
+            creator_id: user.id
+          },
+          user
+        )
+
+      assert Workspaces.list_workspaces_for_user(user) == [workspace1, workspace2]
     end
   end
 
   describe "update_workspace/2" do
     test "updates a workspace with the given attributes" do
+      user = user_fixture()
+      workspace = workspace_fixture(%{creator_id: user.id})
+      update_attrs = %{name: "updated-name"}
+
+      assert {:ok, %Workspace{} = workspace} =
+               Workspaces.update_workspace(workspace, update_attrs)
+
+      assert workspace.name == "updated-name"
+    end
+
+    test "update workspace with invalid data returns error changeset" do
+      user = user_fixture()
+      workspace1 = workspace_fixture(%{creator_id: user.id})
+      workspace2 = workspace_fixture(%{creator_id: user.id})
+
+      assert {:error, %Ecto.Changeset{}} =
+               Workspaces.update_workspace(workspace1, %{name: workspace2})
+
+      assert workspace1 == Workspaces.get_workspace(workspace1.id)
     end
   end
 
   describe "delete_workspace/1" do
-    test "deletes the give workspace" do
+    test "deletes the given workspace" do
+      user = user_fixture()
+      workspace = workspace_fixture(%{creator_id: user.id})
+
+      assert {:ok, %Workspace{}} = Workspaces.delete_workspace(workspace)
+      assert_raise Ecto.NoResultsError, fn -> Workspaces.get_workspace(workspace.id) end
     end
   end
 end
