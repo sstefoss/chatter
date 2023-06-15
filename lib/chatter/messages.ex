37433f767ccd96bd073882bd2179fc561bb3c54e
Stefanos Orovas <stef.orovas@gmail.com>
2023-06-15 17:34:34 +0300

    added PubSub for messages

--- lib/chatter/messages.ex
+++ lib/chatter/messages.ex
@@ -4,6 +4,20 @@ defmodule Chatter.Messages do
   alias Chatter.Models.Channel
   # alias Chatter.Models.Member
 
+  @pubsub Chatter.PubSub
+  @topic inspect(__MODULE__)
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(@pubsub, @topic)
+  end
+
+  def broadcast({:ok, item}, tag) do
+    Phoenix.PubSub.broadcast(@pubsub, @topic, {tag, item})
+    {:ok, item}
+  end
+
+  def broadcast({:error, _changeset} = error, _tag), do: error
+
   def change_message(%Message{} = message, attrs \\ %{}) do
     Message.changeset(message, attrs)
   end
@@ -14,15 +28,18 @@ defmodule Chatter.Messages do
     %Message{}
     |> Message.changeset(attrs)
     |> Repo.insert()
+    |> broadcast(:message_created)
   end
 
   def update_message(%Message{} = message, attrs) do
     message
     |> Message.changeset(attrs)
     |> Repo.update()
+    |> broadcast(:message_updated)
   end
 
-  def delete_message(%Message{} = message), do: Repo.delete(message)
+  def delete_message(%Message{} = message),
+    do: Repo.delete(message) |> broadcast(:message_deleted)
 
   def list_messages_for_channel(%Channel{} = channel) do
     Repo.preload(channel, messages: [:sender]).messages
